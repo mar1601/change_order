6eeca55ec366d562179ef75669668700fddfc3a9
mar1601 <mgarcia2@institutmvm.cat>
2021-11-26 15:45:12 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+  num1, num2 = int(input("Introduce un numero")), int(input("Introduce un numero"))
+
+  num1,num2=num2 , num1
+
+  print("Los valores son ",num1, "y",num2)
+
+if __name__ == '__main__':
+    main()
+
